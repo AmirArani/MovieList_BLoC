29e1390fccf8a2ee9e2519f389038ca8810e6dbe
AmirArani <blackstar.m77@gmail.com>
2022-04-25 19:10:15 +0430

    1401/02/05 | REFACTORING! | part 6 -> delete previous model files

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,12 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:movie_list/data/source/remote/tmdb_api.dart';
-import 'package:movie_list/data/repository/repository.dart';
-import 'package:movie_list/models/movie_entity.dart';
+
 import 'package:movie_list/ui/home/favorites.dart';
 import 'package:movie_list/ui/home/home.dart';
 import 'package:movie_list/ui/home/profile.dart';
 import 'package:movie_list/ui/home/search.dart';
 import 'package:movie_list/ui/theme_data.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -50,9 +47,7 @@ class MyApp extends StatelessWidget {
 
       //inject dependency to TMDB API just here. once and last.
       // now we can use Repository in any class without initializing TMDB API
-      home: Provider<Repository<MovieEntity>>(
-          create: (context) => Repository<MovieEntity>(TmdbAPI()),
-          child: const MainScreen()),
+      home: const MainScreen()
     );
   }
 }
