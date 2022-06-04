eb7c5b17d2ec380202b4f5ed463974dcbeeb8bf2
AmirArani <blackstar.m77@gmail.com>
2022-06-04 18:35:56 +0430

    1401/03/15 | creating artist derail screen -> refactor

--- lib/ui/artist/artist.dart
+++ lib/ui/artist/artist.dart
@@ -39,78 +39,103 @@ class ArtistScreen extends StatelessWidget {
           child: Column(
             children: [
               const SizedBox(height: 124),
-              FutureBuilder(
-                future: personRepository.getImages(id: personEntity.id),
-                builder:
-                    (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
-                  if (snapshot.hasData && snapshot.data != null) {
-                    return CarouselSlider.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (context, index, realIndex) {
-                        return CachedNetworkImage(
-                          imageUrl: 'https://image.tmdb.org/t/p/w185' +
-                              snapshot.data![index],
-                          fadeInCurve: Curves.easeIn,
-                          imageBuilder: (context, imageProvider) => Container(
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: imageProvider, fit: BoxFit.cover),
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(20))),
-                          ),
-                          //TODO: USE SHIMMER IN PLACEHOLDER!!!!
-                          // placeholder: (context, url) =>
-                          //     const CircularProgressIndicator(),
-                        );
-                      },
-                      options: CarouselOptions(
-                        autoPlay: true,
-                        height: 300,
-                        aspectRatio: 0.667,
-                        enlargeCenterPage: true,
-                        enlargeStrategy: CenterPageEnlargeStrategy.scale,
-                        enableInfiniteScroll: true,
-                        initialPage: 0,
-                        viewportFraction: 0.5,
-                        pageSnapping: true,
-                      ),
-                    );
-                  } else {
-                    return Hero(
-                        transitionOnUserGestures: true,
-                        tag: personEntity.id.toString() + 'artist',
-                        child: CachedNetworkImage(
-                          imageUrl: 'https://image.tmdb.org/t/p/w185' +
-                              personEntity.profilePath,
-                          fadeInCurve: Curves.easeIn,
-                          imageBuilder: (context, imageProvider) => Container(
-                            height: 300,
-                            width: 200.1,
-                            decoration: BoxDecoration(
-                                image: DecorationImage(
-                                    image: imageProvider, fit: BoxFit.cover),
-                                borderRadius:
-                                    const BorderRadius.all(Radius.circular(20))),
-                          ),
-                        ));
-                  }
-                },
-              )
-              // Hero(
-              //   transitionOnUserGestures: true,
-              //   tag: personEntity.id.toString() + 'artist',
-              //   child: ClipRRect(
-              //     borderRadius: const BorderRadius.all(Radius.circular(20)),
-              //     child: CachedNetworkImage(
-              //       imageUrl:
-              //           'https://image.tmdb.org/t/p/w185' + personEntity.profilePath,
-              //     ),
-              //   ),
-              // ),
+              ProfilePicturesCarousel(personEntity: personEntity)
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class ProfilePicturesCarousel extends StatelessWidget {
+  const ProfilePicturesCarousel({
+    Key? key,
+    required this.personEntity,
+  }) : super(key: key);
+
+  final PersonEntity personEntity;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: personRepository.getImages(id: personEntity.id),
+      builder: (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+        if (snapshot.hasData && snapshot.data != null) {
+          return Carousel(items: snapshot.data);
+        } else {
+          return Hero(
+            transitionOnUserGestures: true,
+            tag: personEntity.id.toString() + 'artist',
+            child: CachedNetworkImage(
+              imageUrl: 'https://image.tmdb.org/t/p/w185' + personEntity.profilePath,
+              fadeInCurve: Curves.easeIn,
+              imageBuilder: (context, imageProvider) => Container(
+                height: 300,
+                width: 200.1,
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+                    borderRadius: const BorderRadius.all(Radius.circular(20))),
+              ),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
+
+class Carousel extends StatelessWidget {
+  final List<String>? items;
+
+  const Carousel({
+    Key? key,
+    required this.items,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider.builder(
+      itemCount: items?.length,
+      itemBuilder: (context, index, realIndex) {
+        return CarouselItem(item: items![index]);
+      },
+      options: CarouselOptions(
+        autoPlay: true,
+        height: 300,
+        aspectRatio: 0.667,
+        enlargeCenterPage: true,
+        enlargeStrategy: CenterPageEnlargeStrategy.scale,
+        enableInfiniteScroll: true,
+        initialPage: 0,
+        viewportFraction: 0.5,
+        pageSnapping: true,
+      ),
+    );
+  }
+}
+
+class CarouselItem extends StatelessWidget {
+  const CarouselItem({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final String item;
+
+  @override
+  Widget build(BuildContext context) {
+    return CachedNetworkImage(
+      imageUrl: 'https://image.tmdb.org/t/p/w185' + item,
+      fadeInCurve: Curves.easeIn,
+      imageBuilder: (context, imageProvider) => Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+            borderRadius: const BorderRadius.all(Radius.circular(20))),
+      ),
+      //TODO: USE SHIMMER IN PLACEHOLDER!!!!
+      // placeholder: (context, url) =>
+      //     const CircularProgressIndicator(),
     );
   }
 }
